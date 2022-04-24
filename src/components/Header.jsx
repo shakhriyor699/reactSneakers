d47f7a7b44c6aac9497f6dca678f762302d6ca5d
shakhriyor348 <62033235+shakhriyor348@users.noreply.github.com>
2022-04-24 16:40:16 +0300

    final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = (props) => {
         <header className="d-flex justify-between align-center p-40">
             <Link to={"/"}>
                 <div className="headerLeft d-flex align-center">
-                    <img width={40} height={40} src="/img/logo.svg" alt="Logo" className="mr-15" />
+                    <img width={40} height={40} src="img/logo.svg" alt="Logo" className="mr-15" />
                     <div>
                         <h3 className="text-uppercase">REACT SNEAKERS</h3>
                         <p>Магазин лучших кроссовок</p>
@@ -21,17 +21,17 @@ const Header = (props) => {
             </Link>
             <ul className="headerRight d-flex align-center">
                 <li className="mr-30 d-flex align-center cu-p" onClick={props.onClickCart}>
-                    <img width={18} height={18} src="/img/cart.svg" alt="Cart" className="mr-10" />
+                    <img width={18} height={18} src="img/cart.svg" alt="Cart" className="mr-10" />
                     <span>{totalPrice} руб.</span>
                 </li>
                 <li className="mr-30 d-flex cu-p">
                     <Link to={'/favorites'}>
-                        <img width={18} height={18} src="/img/heart.svg" alt="Favorite" />
+                        <img width={18} height={18} src="img/heart.svg" alt="Favorite" />
                     </Link>
                 </li>
                 <li className="d-flex">
                     <Link to={'/orders'}>
-                        <img width={18} height={18} src="/img/user.svg" alt="User" />
+                        <img width={18} height={18} src="img/user.svg" alt="User" />
                     </Link>
                 </li>
             </ul>
